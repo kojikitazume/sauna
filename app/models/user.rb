7a536a423cfd030ba048075018528b5fde3be0f0
北爪康司 <navigator.lp720@gmail.com>
2024-07-31 22:10:35 +0900

    deviseの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :sauna_visits
     has_many :bookmarks
     has_many :bookmarked_saunas, through: :bookmarks, source: :sauna
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'saunas#index'
   
   resources :saunas, only: [:index, :show] do
