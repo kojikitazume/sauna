509259b331b6f5fe361e4dbddf8379955a8845a1
北爪康司 <navigator.lp720@gmail.com>
2024-08-07 23:03:15 +0900

    トップページの見た目の修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,19 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 // メニューを開くトグル機能を追加
 document.addEventListener('DOMContentLoaded', () => {
-  const toggler = document.querySelector('.navbar-toggler');
-  if (toggler) {
+  const togglers = document.querySelectorAll('.navbar-toggler');
+  togglers.forEach(toggler => {
     toggler.addEventListener('click', () => {
-      const menu = document.querySelector('.dropdown-menu');
-      if (menu) {
-        menu.classList.toggle('show');
-      }
+      const menu = toggler.nextElementSibling;
+      menu.classList.toggle('show');
     });
-  }
+  });
 });
